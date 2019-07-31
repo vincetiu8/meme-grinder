d638ac434a292e73a440ff7efd83d20526a0e6e6
Vinc <vincetiu8@gmail.com>
2019-07-30 21:15:54 -0400

    Update reddit bot to use selenium to load page

--- fetch_memes/reddit.py
+++ fetch_memes/reddit.py
@@ -1,17 +1,79 @@
-import requests
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import shutil
-from bs4 import BeautifulSoup
+import os
+import time
+import requests
 
-url = 'https://www.reddit.com/r/memes/'
-user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
-headers = {'User-Agent': user_agent}
-page = requests.get(url, headers=headers)
-soup = BeautifulSoup(page.text, 'html.parser')
-for a in soup.find_all('a'):
-    if a.img:
-        print(a.img['src'])
-        url = a.img['src']
+class Reddit_Bot():
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def init_directory(self):
+        if not os.path.isdir('./images'):
+            os.mkdir('./images')
+
+    def load_hyperlinks(self):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_experimental_option('prefs', {'profile.default_content_setting_values.notifications' : 2})
+
+        # For testing functionality
+        # chrome_options.add_experimental_option("detach", True)
+
+        driver = webdriver.Chrome('D:/Vince/Documents/chromedriver.exe', chrome_options=chrome_options)
+        driver.get('https://www.reddit.com/login/')
+
+        time.sleep(1.5)
+        emailInput = driver.find_elements_by_css_selector('form fieldset input')[0]
+        passwordInput = driver.find_elements_by_css_selector('form fieldset input')[1]
+        emailInput.send_keys(self.username)
+        passwordInput.send_keys(self.password)
+        passwordInput.send_keys(Keys.ENTER)
+
+        time.sleep(5)
+        links = driver.find_elements_by_css_selector('a')
+        self.loop_through_links(links)
+
+        driver.quit()
+
+    def loop_through_links(self, links):
+        for link in links:
+            print(link.get_attribute('href'))
+            if (link.get_attribute('href').find('https://www.reddit.com/r/') == 0
+            ):
+                prevlink = link.get_attribute('href')
+                try:
+                    img = link.find_element_by_css_selector('img')
+                except:
+                    continue
+                if img.get_attribute('alt').find('Post image') == -1:
+                    continue
+                url = img.get_attribute('src')
+                if self.download_image(url) == False:
+                    return False
+        return True
+
+    def download_image(self, url):
+        id = url[url.find('it/') + 3:url.find('?wi') - 3]
+        if self.find(id):
+            return False
+        print(id)
         response = requests.get(url, stream=True)
-        with open('img.png', 'wb') as out_file:
+        with open('./images/' + id + '.jpg', 'wb') as out_file:
             shutil.copyfileobj(response.raw, out_file)
         del response
+        return True
+
+
+    def find(self, id):
+        for filename in os.listdir('images'):
+            if filename.startswith(id):
+                return True
+
+        return False
+
+# For testing functionality
+bot = Reddit_Bot('invincbot', 'frenchfri')
+bot.init_directory()
+bot.load_hyperlinks()
